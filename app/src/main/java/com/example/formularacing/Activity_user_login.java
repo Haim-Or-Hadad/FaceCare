c2f22d32758f0b9a39dd582d1f036b1d0f2e1f4b
haim <haimor1123@gmail.com>
2022-11-03 12:22:40 +0200

    first commit

---
+++ app/src/main/java/com/example/formularacing/Activity_user_login.java
@@ -0,0 +1,14 @@
+package com.example.formularacing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_user_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}
---
+++ app/src/main/java/com/example/formularacing/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.formularacing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //click on admin button to login
+        Button adminButton = (Button) findViewById(R.id.admin_button);
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+        //click on user button to login
+        Button userButton = (Button) findViewById(R.id.user_button);
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity_user_login();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+    public void openActivity_user_login(){
+        Intent intent = new Intent(this, Activity_user_login.class);
+        startActivity(intent);
+    }
+    }
