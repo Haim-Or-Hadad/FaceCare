d8e470cef4ef446b0d5d8cddb84c6b0f1261a0db
haim <haimor1123@gmail.com>
2022-12-01 20:47:41 +0200

    no circle_

--- app/src/main/java/com/example/formularacing/client_order.java
+++ app/src/main/java/com/example/formularacing/client_order.java
@@ -9,6 +9,6 @@ public class Activity_user_login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_login);
+        setContentView(R.layout.client_order_screen);
     }
 }
