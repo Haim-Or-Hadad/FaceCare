60b8ba9c80c866d5a25ab80f008aa14a2c870474
Raz <razgavrieli@gmail.com>
2022-12-01 22:02:34 +0200

    fix bugs: 1. changed a name of a file to fit the class 2. changed R.layout.activity_2 to R.layout.admin_screen to fit the existing layouts 3. added circle_button.xml to fix build problem

--- app/src/main/java/com/example/formularacing/Activity2.java
+++ app/src/main/java/com/example/formularacing/Activity2.java
@@ -2,10 +2,8 @@ package com.example.formularacing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +14,7 @@ public class Activity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.admin_screen);
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
 
--- app/src/main/java/com/example/formularacing/MainActivity.java
+++ app/src/main/java/com/example/formularacing/MainActivity.java
@@ -6,11 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
     @Override
