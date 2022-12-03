70a148931ca4f4a3191cc5e21a41727eb24ccc98
haim <haimor1123@gmail.com>
2022-12-03 15:45:11 +0200

    gui

--- app/src/main/java/com/example/formularacing/Activity_user_login.java
+++ app/src/main/java/com/example/formularacing/Activity_user_login.java
@@ -1,14 +1,121 @@
 package com.example.formularacing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.ListView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity_user_login extends AppCompatActivity {
-
+    /**
+     * @param calanderView
+     *        to initialize the calander to pick a date fron client
+     * @param bearButton
+     *        beard_button to initialize the button that save the beard treatment
+     * @param haircutButton
+     *        haircutButton to initialize the button that save the haircut treatment
+     * @param acneButton
+     *        acne_button to initialize the button that save the acne treatment
+     * @param beardButton
+     *        beard_button to initialize the button that save the beard treatment
+     * @param classic_facial
+     *        beard_button to initialize the button that save the classic facial treatment
+     * @param calanderView
+     *        initialize the calander view element
+     * @param listView
+     *        time slot to pick a appointment
+     */
+    CalendarView calendarView;
+    Button beardButton;
+    Button haircutButton;
+    Button acneButton;
+    Button classicFacial;
+    String selectedTreatment;
+    ListView listView;
+    List<String> slots;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.client_order_screen);
+        /**
+         * detects the id of the beard treatment button and saves the selection if the button is pressed
+         */
+        beardButton = (Button) findViewById(R.id.beard);
+        beardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTreatment = "beard";
+            }
+        });
+        /**
+         * detects the id of the haircut treatment button and saves the selection if the button is pressed
+         */
+        haircutButton = (Button) findViewById(R.id.haircut);
+        haircutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTreatment = "haircut";
+            }
+        });
+        /**
+         * detects the id of the acne treatment button and saves the selection if the button is pressed
+         */
+        acneButton = (Button) findViewById(R.id.acne);
+        acneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTreatment = "acne";
+            }
+        });
+        /**
+         * detects the id of the classic facial treatment button and saves the selection if the button is pressed
+         */
+        classicFacial = (Button) findViewById(R.id.classic_facial);
+        classicFacial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTreatment = "facial";
+            }
+        });
+
+        //save the date of calander picker
+        calendarView = (CalendarView) findViewById(R.id.calendarView2);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 + 1) + "/" + i2 + "/" + i;
+            }
+        });
+
+    listView = (ListView) findViewById(R.id.listView);
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("first hour");
+        arrayList.add("second hour");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+        //getslots();
     }
+//    private void getslots(){
+//        try {
+//            String format = "yyyy-MM-dd HH:mm";
+//            SimpleDateFormat sdf = new SimpleDateFormat(format);
+//
+//        }catch (){
+//
+//        }
+//
+//    }
+
+    //function that show the available hours to choose appointenment
+
 }
--- app/src/main/java/com/example/formularacing/MainActivity.java
+++ app/src/main/java/com/example/formularacing/MainActivity.java
@@ -1,17 +1,77 @@
 package com.example.formularacing;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * @param userPhone
+     * parameter to save the phone of the user
+     */
+    EditText userPhone;
+    String phoneNumber;
+    ImageView whatsapp,facebook,instagram;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assign variable
+        whatsapp = findViewById(R.id.whatsapp);
+        facebook = findViewById(R.id.facebook);
+        instagram = findViewById(R.id.instagram);
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //initialize mobile number with country code
+                String sNumber = "+972549761170";
+                //initialize uri
+                Uri uri = Uri.parse(String.format("https://api.whatsapp.com/send?phone=%s", sNumber));
+                //init intent
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                //set data
+                intent.setData(uri);
+                //set flag
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //
+                startActivity(intent);
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sAppLink = "fb://page/237564710351658";
+                String sPackage = "com.facebook.katana";
+                String sWebLink = "https://www.facebook.com/12H1rDeveloper";
+                //create method
+                openLink(sAppLink, sPackage, sWebLink);
+            }
+        });
+
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sAppLink = "https://www.instagram.com";
+                String sPackage = "com.instagram.android";
+                openLink(sAppLink, sPackage, sAppLink);
+            }
+        });
+
+
+        userPhone = (EditText) findViewById(R.id.user_phone);
         //click on admin button to login
         Button adminButton = (Button) findViewById(R.id.admin_button);
         adminButton.setOnClickListener(new View.OnClickListener() {
@@ -25,17 +85,73 @@ public class MainActivity extends AppCompatActivity {
         userButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity_user_login();
+                phoneNumber = userPhone.getText().toString();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("alert");
+                builder.setMessage("please enter a phone number");
+                builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                if (phoneNumber.isEmpty()) {
+                    builder.show();
+                } else {
+                    //need to add a function that send to ilan and raz the phone number
+                    openActivity_user_login();
+                }
             }
         });
     }
 
-    public void openActivity2() {
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
+    /**
+     * function that send to DAL request to get a details about user according his phone number
+     * return String
+     */
+    public String get_currnt_appointments(int phoneNumber) {
+        return "a";
     }
-    public void openActivity_user_login(){
-        Intent intent = new Intent(this, Activity_user_login.class);
-        startActivity(intent);
+
+
+        public void openActivity2 () {
+            Intent intent = new Intent(this, Activity2.class);
+            startActivity(intent);
+        }
+        public void openActivity_user_login () {
+            Intent intent = new Intent(this, Activity_user_login.class);
+            startActivity(intent);
+        }
+
+    /**
+     *
+     * @param sAppLink enter to the app if exist
+     * @param sPackage
+     * @param sWebLink enter to facebook page in browser
+     */
+    private void openLink(String sAppLink,String sPackage,String sWebLink){
+        try {
+            //init uri
+            Uri uri = Uri.parse(sAppLink);
+            //init intent
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            //set data
+            intent.setData(uri);
+            //set package
+            intent.setPackage(sPackage);
+            //set flag
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }catch (ActivityNotFoundException activityNotFoundException){
+            Uri uri = Uri.parse(sWebLink);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(uri);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+
     }
-    }
+}
+
+
