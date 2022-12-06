78bcb3f858cd6e9989f7cff33ab84fd52d24ca84
IlanG96 <goldlmilan@gmail.com>
2022-12-06 15:25:09 +0200

    getAvailableTimes tests

--- app/src/main/java/com/example/formularacing/dataAccess.java
+++ app/src/main/java/com/example/formularacing/dataAccess.java
@@ -1,5 +1,7 @@
 package com.example.formularacing;
 
+import static android.content.ContentValues.TAG;
+
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -7,18 +9,24 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+import javax.security.auth.callback.Callback;
 
 public class dataAccess {
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance("https://test3-a0cfd-default-rtdb.europe-west1.firebasedatabase.app/");
     public Map<String, List<String>> existingAppointments = new HashMap<>();
+    public Map<String, List<String>> availableAppointments = new HashMap<>();
     public dataAccess() {
     // empty constructor
     }
@@ -60,9 +68,9 @@ public class dataAccess {
         return null;
     }
 
-    public Map<String, List<String>> scheduleAppointment(String date,String time){
+    public Map<String, List<String>> scheduleAppointment(String phoneNumber,String date,String time){
         // Create a map to store the Date objects
-        DatabaseReference myRef = database.getReference("OpenAppoint");
+        DatabaseReference myRef = database.getReference("OpenAppointment");
         Map<String, List<String>> newAppointmentDate = new HashMap<>();
         List<String> appointmentTime = new ArrayList<>();
         // Add the Date objects to the map with unique keys
@@ -74,7 +82,7 @@ public class dataAccess {
                 //need to check if the time is open for reservation
                 task.getResult();//or times or null\
 
-                database.getReference().child("users").child("0527142312").setValue(newAppointmentDate);
+                database.getReference().child("users").child(phoneNumber).setValue(newAppointmentDate);
 
             }
         });
@@ -83,6 +91,57 @@ public class dataAccess {
         return null;
     }
 
+    // Create a function that retrieves data from the database and updates a list
+    public void getAvailableTimes(String wantedDate) {
+        List<String> times=new ArrayList<>();
+        DatabaseReference appointmentsRef = database.getReference("OpenAppointment");
+        // Save the list of times to the availableAppointments map
+        availableAppointments.put(wantedDate, times);
+
+        // Use the `child()` method to get a reference to the child node with the specified date
+        DatabaseReference dateRef = appointmentsRef.child(wantedDate);
+
+        dateRef.get()
+                .addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            // Retrieve the data and add it to the list
+                            DataSnapshot dataSnapshot = task.getResult();
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                times.add((String) snapshot.getValue());
+                            }
+
+                            // Save the list of times to the availableAppointments map
+                            availableAppointments.put(wantedDate, times);
+                        } else {
+                            // Handle error
+                            Log.e(TAG, "Failed to retrieve data: " + task.getException().getMessage());
+                        }
+                    }
+                });
+    }
+
+
+
+
+
+
+
+
+//    public void adminSetWorkingTimes(String date,String time){
+//        DatabaseReference myRef = database.getReference("OpenAppointment");
+//        AppointmentCreator newAppointment= new AppointmentCreator(date);
+//        newAppointment.setTime(time);
+//        myRef.child(date).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DataSnapshot> task) {
+//                task.getResult();//or times or null\
+//                myRef.child(newAppointment.getDate()).setValue(newAppointment.getAvailableTimes());
+//
+//            }
+//        });
+//    }
 
     public void t() {
     // Write a message to the database
