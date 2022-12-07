62175601dea17654a40897420b641a6070476961
haim <haimor1123@gmail.com>
2022-12-07 11:08:10 +0200

    admin change

--- app/src/main/java/com/example/formularacing/admin_view.java
+++ app/src/main/java/com/example/formularacing/admin_view.java
@@ -2,7 +2,9 @@ package com.example.formularacing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class admin_view extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class admin_view extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_view);
+
+        //when workplan button clicked
+        Button workplanButton = (Button) findViewById(R.id.weekly_workplan);
+        workplanButton.setOnClickListener((view)->openWorkplanActivity());
+
+        //when my shifts button clicked
+        Button shiftsButton = (Button) findViewById(R.id.myShifts);
+        shiftsButton.setOnClickListener((view)-> showShifts());
+
+    }
+
+    private void showShifts() {
+        Intent intent = new Intent(admin_view.this, my_shifts.class);
+        admin_view.this.startActivity(intent);
+        //need add here function that get the the work days of employee
+    }
+
+    public void openWorkplanActivity(){
+        Intent intent = new Intent(admin_view.this, workplanActivity.class);
+        admin_view.this.startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/formularacing/my_shifts.java
@@ -0,0 +1,14 @@
+package com.example.formularacing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class my_shifts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_shifts);
+    }
+}
