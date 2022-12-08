9db88edf3c9db81d9d1453f558a34be40e62b829
haim <haimor1123@gmail.com>
2022-12-08 14:54:57 +0200

    " "

--- app/src/main/java/com/example/formularacing/manageAppointments.java
+++ app/src/main/java/com/example/formularacing/manageAppointments.java
@@ -3,6 +3,7 @@ package com.example.formularacing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,11 +14,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class manageAppointments extends AppCompatActivity {
     List<String> allAppointments = new ArrayList<String>();
-    String[] mobileTypes = {"0549761170:10:30","0549761170:10:30","0549761170:10:30","0549761170:10:30"};
+    List<String> mobileTypes = new ArrayList<String>();
+
     dataAccess dal = new dataAccess();
 
     @Override
@@ -30,56 +33,61 @@ public class manageAppointments extends AppCompatActivity {
         ListView acneAppointments = findViewById(R.id.acne_listview);
         ListView haircutAppointments = findViewById(R.id.haircut_listview);
 
-        Task test =dal.adminShowAppointment("12-12-2022");
+        Task task =dal.adminShowAppointment("14-12-2022");
         //Progress.setVisibility(View.VISIBLE);
-        while (!test.isComplete()){
+        while (!task.isComplete()){
 
         }
         //Progress.setVisibility(View.INVISIBLE);
-        List<AppointmentCreator> appointmentList = (List<AppointmentCreator>)((DataSnapshot)test.getResult()).getValue();
-//        for (int i =0; i< appointmentList.size(); i++) {
-//            if (appointmentList.toArray()[i].toString().equals("beard")){
-//                mobileTypes[i] = appointmentList.get(i).getDate();
-//            }
-//            else{
-//                continue;
-//            }
-//        }
+
+        //List<AppointmentCreator> appointmentList = (List<AppointmentCreator>)((DataSnapshot)task.getResult()).getValue();
+        List<HashMap<String, String>> appointmentCreatorList = (List<HashMap<String, String>>)((DataSnapshot) task.getResult()).getValue();
+        for (int i = 0; i<appointmentCreatorList.size(); i++) {
+            AppointmentCreator currAppointment = new AppointmentCreator(appointmentCreatorList.get(i));
+            mobileTypes.add(currAppointment.getTime() + " " + currAppointment.getPhone());
+        }
 
         //Array Adapter
-        mobileTypes[0]= "Beard appointments";
+        //mobileTypes[0]= "Beard appointments";
         ArrayAdapter beardAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 mobileTypes
         );
-        mobileTypes[0]= "Acne appointments";
+        beardAppointments.setAdapter(beardAdapter);
+        //mobileTypes[0]= "Acne appointments";
         ArrayAdapter acneAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 mobileTypes
         );
-        mobileTypes[0]= "Facial appointments";
+        acneAppointments.setAdapter(acneAdapter);
+        //mobileTypes[0]= "Facial appointments";
         ArrayAdapter facialAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 mobileTypes
         );
-        mobileTypes[0]= "Haircut appointments";
+        facialAppointments.setAdapter(facialAdapter);
+        //mobileTypes[0]= "Haircut appointments";
         ArrayAdapter haircutAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 mobileTypes
         );
-        beardAppointments.setAdapter(beardAdapter);
-        acneAppointments.setAdapter(acneAdapter);
-        facialAppointments.setAdapter(facialAdapter);
         haircutAppointments.setAdapter(haircutAdapter);
 
         // Handiling Click Events in ListView
         beardAppointments.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Task task =dal.cancelAppointment("14-12-2022","15:00","0549761170");
+                //Progress.setVisibility(View.VISIBLE);
+                //while (!task.isComplete()){
+
+                //}
+                //beardAdapter.remove(position);
+                //beardAdapter.notifyDataSetChanged();
                 Toast.makeText(manageAppointments.this,
                                 "appointment cancaled " + beardAdapter.getItem(position),
                                 Toast.LENGTH_SHORT).show();
