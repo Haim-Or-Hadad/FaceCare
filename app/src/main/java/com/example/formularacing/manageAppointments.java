77c75848bc7c350e93f7120f7d55a6f63eb0a07a
haim <haimor1123@gmail.com>
2022-12-10 20:05:04 +0200

    "manageAppointments working"

--- app/src/main/java/com/example/formularacing/manageAppointments.java
+++ app/src/main/java/com/example/formularacing/manageAppointments.java
@@ -3,6 +3,7 @@ package com.example.formularacing;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.MessagePattern;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,12 +11,14 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -24,12 +27,14 @@ public class manageAppointments extends AppCompatActivity {
     CalendarView calendarView;
     dataAccess dal = new dataAccess();
     String date;
+    ProgressBar Progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_appointments);
         calendarView = findViewById(R.id.calendarView3);
         ListView Appointments = findViewById(R.id.beard_listview);
+        Progress = findViewById(R.id.progressBar2);
 /**
  * When the customer chooses a date, so a list of available hours appears to him with
  * listview that use arrayAddapter to show the slots.
@@ -37,6 +42,7 @@ public class manageAppointments extends AppCompatActivity {
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                    //allAppointments  = Collections.<String>emptyList();
                     date = i2 + "-" + (i1 + 1)  + "-" + i;
                     Task task =dal.adminShowAppointment(date);
 
@@ -46,40 +52,34 @@ public class manageAppointments extends AppCompatActivity {
                     }
                 //Progress.setVisibility(View.INVISIBLE);
                 List<HashMap<String, String>> appointmentCreatorList = (List<HashMap<String, String>>)((DataSnapshot) task.getResult()).getValue();
-                for (int j = 0; j<appointmentCreatorList.size(); j++) {
-                    AppointmentCreator currAppointment = new AppointmentCreator(appointmentCreatorList.get(j));
-                    allAppointments.add(currAppointment.getTime() + " " + currAppointment.getPhone());
+                if(appointmentCreatorList == null) {
+                allAppointments = Collections.<String>emptyList();
                 }
+                else {
+                    for (int j = 0; j<appointmentCreatorList.size(); j++) {
+                        AppointmentCreator currAppointment = new AppointmentCreator(appointmentCreatorList.get(j));
+                        String str = currAppointment.getTime().toString();
+                        allAppointments.add(str);
+                    }
+                    }
                 ArrayAdapter arrayAdapter = new ArrayAdapter(manageAppointments.this, R.layout.text_style_list, allAppointments);
                 Appointments.setAdapter(arrayAdapter);
             }
         });
 
 
-        // Handiling Click Events in ListView
-//        Appointments.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Task task =dal.cancelAppointment("12-12-2022","09:30","0549761171");
-//                //Progress.setVisibility(View.VISIBLE);
-//                while (!task.isComplete()){
-//
-//                }
-//                //adapter.remove(position);
-//                //adapter.notifyDataSetChanged();
-//                Toast.makeText(manageAppointments.this,
-//                                "appointment cancaled " ,
-//                                Toast.LENGTH_SHORT).show();
-//            }
-//        });
         Appointments.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-                Task task =dal.cancelAppointment(date,"09:30","0549761171");
+                String time = Appointments.getItemAtPosition(i).toString().substring(0,5);
+                //String phoneNumber = Appointments.getItemAtPosition(i).toString().substring(5,15);
+                Task task =dal.cancelAppointment(date,time,"0549761170");
                 //Progress.setVisibility(View.VISIBLE);
-                //while (!task.isComplete()){
-
-                //}
+//                while (!task.isComplete()){
+//
+//                }
+//                DataSnapshot test=(DataSnapshot)task.getResult();
+                //Progress.setVisibility(view.INVISIBLE);
                 //adapter.remove(position);
                 //adapter.notifyDataSetChanged();
                 Toast.makeText(manageAppointments.this,
