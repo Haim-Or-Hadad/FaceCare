cdfb3c37ca2aef892453fbfcc99028c933f26baf
haim <haimor1123@gmail.com>
2023-01-10 18:12:36 +0200

    comments added

--- app/src/main/java/com/example/formularacing/AdminServices.java
+++ app/src/main/java/com/example/formularacing/AdminServices.java
@@ -1,87 +1,98 @@
 package com.example.formularacing;
 
-import static com.example.formularacing.MainScreen.phoneNumber;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Service;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.Task;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-
 public class AdminServices extends AppCompatActivity {
-    Button haircutService ;
-    Button acneService ;
-    Button beardService ;
-    Button faceCareService ;
-    Button perfectService ;
-    Button addServices;
-    Button currentServices;
-    Button resetServices ;
-    List<String> services = new ArrayList<>();
+    //declare buttons for each service
+    private Button haircutService;
+    private Button acneService;
+    private Button beardService;
+    private Button faceCareService;
+    private Button perfectService;
+    private Button addServices;
+    private Button currentServices;
+    private Button resetServices;
+
+    //create a list to hold the selected services
+    private List<String> services = new ArrayList<>();
+    //create an instance of the dataAccess class
     dataAccess dal = new dataAccess();
-    //dataAccess dal = new dataAccess(MainScreen.phoneNumber);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_services);
-        haircutService = (Button) findViewById(R.id.haircutService);
-        acneService = (Button) findViewById(R.id.acneService);
-        beardService = (Button) findViewById(R.id.beardService);
-        faceCareService = (Button) findViewById(R.id.faceCareService);
-        perfectService = (Button) findViewById(R.id.perfectService);
-        addServices = (Button) findViewById(R.id.addServices);
-        currentServices = (Button) findViewById(R.id.currentServices);
-        resetServices = (Button) findViewById(R.id.resetServices);
+        //find views by id
+        haircutService = findViewById(R.id.haircutService);
+        acneService = findViewById(R.id.acneService);
+        beardService = findViewById(R.id.beardService);
+        faceCareService = findViewById(R.id.faceCareService);
+        perfectService = findViewById(R.id.perfectService);
+        addServices = findViewById(R.id.addServices);
+        currentServices = findViewById(R.id.currentServices);
+        resetServices = findViewById(R.id.resetServices);
 
 
-        haircutService.setOnClickListener((view)->addToServices("haircut"));
-        acneService.setOnClickListener((view)->addToServices("acne"));
-        beardService.setOnClickListener((view)->addToServices("beard"));
-        faceCareService.setOnClickListener((view)->addToServices("facecare"));
-        perfectService.setOnClickListener((view)->addToServices("perfect"));
-        addServices.setOnClickListener((view)->sendToDal());
-        currentServices.setOnClickListener((view)->manageServices("current"));
-        resetServices.setOnClickListener((view)->manageServices("reset"));
+        //set onClickListener for each service button
+        haircutService.setOnClickListener(view -> addToServices("haircut"));
+        acneService.setOnClickListener(view -> addToServices("acne"));
+        beardService.setOnClickListener(view -> addToServices("beard"));
+        faceCareService.setOnClickListener(view -> addToServices("facecare"));
+        perfectService.setOnClickListener(view -> addToServices("perfect"));
 
+        //set onClickListener for adding and viewing selected services
+        addServices.setOnClickListener(view -> sendToDal());
+        currentServices.setOnClickListener(view -> showCurrentServices());
+
+        //set onClickListener for resetting selected services
+        resetServices.setOnClickListener(view -> resetServices());
     }
 
+    // Method for sending selected services to the Data Access Layer
     private void sendToDal() {
-        for (int i = 0; i < 3 ; i++){
-            service tempService = new service("test",services.get(i).toString(),"test");
-            dal.setService(tempService,i);
+        if (services.isEmpty()) {
+            Toast.makeText(this, "No services selected!", Toast.LENGTH_SHORT).show();
+            return;
         }
-
+        else if(services.size() < 4) {
+            Toast.makeText(this, "please select 4 services", Toast.LENGTH_SHORT).show();
+        }
+        else{
+        for (int i = 0; i < 3 && i < services.size(); i++) {
+            service tempService = new service("test", services.get(i), "test");
+            dal.setService(tempService, i);
+        }
+            Toast.makeText(this, "services selected and display in user screen", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    private void manageServices(String str) {
-        if(str.equals("current")){
+    // method for showing the current selected services
+    private void showCurrentServices() {
+        if (services.isEmpty()) {
+            Toast.makeText(this, "No services selected!", Toast.LENGTH_SHORT).show();
+        } else {
             String selectedServices = String.join(", ", services);
-            Toast.makeText(AdminServices.this, selectedServices, Toast.LENGTH_SHORT).show();
-        }
-        else {
-            services.clear();
+            Toast.makeText(this, selectedServices, Toast.LENGTH_SHORT).show();
         }
     }
 
+    // method for resetting the services list
+    private void resetServices() {
+        services.clear();
+        Toast.makeText(this, "services deleted", Toast.LENGTH_SHORT).show();
+    }
+
+    // Method for adding service to the services list
     private void addToServices(String type) {
-        if(services.isEmpty()){
-            services.add(type);
+        if (services.size() >= 4) {
+            Toast.makeText(this, "You can only select up to 4 services", Toast.LENGTH_SHORT).show();
+            return;
         }
-        else if ( services.size() < 4){
-            services.add(type);
-        }
-        else {
-            String show = "please click on reset button and choose 4 treatment";
-            Toast toast = Toast.makeText(AdminServices.this, show , Toast.LENGTH_SHORT);
-            toast.show();
-        }
+        services.add(type);
     }
 }
