4975388b5dbc7d18ee77a495a3c3c39e817f8a48
Raz <razgavrieli@gmail.com>
2023-01-17 22:43:07 +0200

    bug fixes in xml files and dataAccess

--- app/src/main/java/com/example/formularacing/dataAccess.java
+++ app/src/main/java/com/example/formularacing/dataAccess.java
@@ -17,6 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -268,6 +269,7 @@ public class dataAccess {
             public void onComplete(@NonNull Task<DataSnapshot> task) {
                 List<String> times = (List<String>) task.getResult().getValue();
                 times.add(time);
+                Collections.sort(times);
                 openAppointRef.child(date).setValue(times);
             }
         });
